e35511a27be7f9ea1817348ec8d28ae92ee5e69e
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2025-04-23 14:08:50 +0300

    postgre-setup

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://inventory_manager_s9lo_user:ih2D3zpTmmYmypQu9p1PjBBz4QtvDhwR@dpg-d04chqp5pdvs73c81l2g-a/inventory_manager_s9lo'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
